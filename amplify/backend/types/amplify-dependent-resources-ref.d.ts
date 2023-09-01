86d14b0b44d388ba4db5037bb888553031031393
root <root@DESKTOP-NFFQUMS>
2023-09-01 09:42:49 +0100

    update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mdropbox7fedefde": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './components/form';
 import Files from './components/files';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator
+} from "@aws-amplify/ui-react";
 
-function App() {
+import { Amplify, Auth } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+
+function App({ signOut }) {
   const [filename, setFilename] = useState("")
+  const [access_token, setToken] = useState("")
+  const [username, setUsername] = useState("")
 
   const getFilename = (val) => {
     setFilename(val)
-  
   }
+
+  useEffect(() => {
+    const user = Auth.currentAuthenticatedUser()
+    const token = user.signInUserSession.accessToken
+    console.log(user.username)
+    console.log(token)
+    //setToken(token)
+    //setUsername(user.username)
+  })
+
+  useEffect(() => {
+    const token_value = {
+      'access_token': access_token
+    }
+    const loadfile = async () => {
+      try {
+        const res = await fetch("https://pimvhp4mb5.execute-api.eu-north-1.amazonaws.com/dev/savedfiles",
+          {
+            method: 'post',
+            body: JSON.stringify(token_value)
+          })
+        if (res.ok) {
+          console.log(res)
+        } else {
+          console.log("files file to load")
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    loadfile();
+
+  }, [])
+
   return (
     <>
+      <div>welcome to your dashboard {username}</div>
       <div>
         <p>{filename}</p>
       </div>
       <Form filename={getFilename} />
-      <Files/>
+      <Files />
+      <p>
+        <button onClick={signOut}>Logout</button>
+      </p>
     </>
-     
+
   );
 }
 
-export default App;
+export default withAuthenticator(App);
